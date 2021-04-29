bf317b7efdcc3c25b18a3ad9c8150217cbc6ed9e
Kristen Groller <groller@rogers.com>
2021-04-29 06:01:29 -0400

    react added

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -30,6 +30,7 @@ type User {
 
 }
   type Query {
+    me: [User]
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
@@ -39,11 +40,15 @@ type User {
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
 
   type Auth {
     token: ID!
     user: User
+    
   }
 `;
 
--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const { authMiddleware } = require('./utils/auth');
+const path = require('path');
+
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -10,7 +14,8 @@ const app = express();
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate our Apollo server with the Express application as middleware
